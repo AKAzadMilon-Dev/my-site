62e3416bf504b479c6858c677d937065dfb6f255
AK Azad Milon <akazadmilon.dev@gmail.com>
2023-02-08 12:07:06 +0600

    programatic usage router navigation on client site

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,13 @@
+'use client'
 import Link from "next/link"
+import { useRouter } from "next/navigation";
 
 export default function Contact(){
+    const router = useRouter()
     return <div>
         <h1>contact</h1>
         <Link href="/">Home</Link>
         <Link href={`/blog/kjkjk`}>Post</Link>
+        <button type="button" onClick={()=> router.push('/')}>home</button>
     </div>
 }
